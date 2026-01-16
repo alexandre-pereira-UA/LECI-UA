b89147bdb88b729634be6cec6db579a8e3aa203a
alexandre-pereira-UA <alexandre.rp@ua.pt>
2026-01-16 21:19:38 +0000

    fghfs

---
+++ 3ºano/1ºsemestre/IA/Prática/Guião do tratamento da incerteza/mdp.py
@@ -0,0 +1,11 @@
+
+class MDP:
+
+    def __init__(self,world,gama,epsilon):
+        self.world = world
+        self.gama = gama
+        self.epsilon = epsilon
+
+    def value_iteration(self):
+        pass
+
---
+++ 3ºano/1ºsemestre/IA/Prática/Guião do tratamento da incerteza/sof2026h.py
@@ -0,0 +1,9 @@
+from bayes_net import *
+
+bn = BayesNet()
+
+variables = ['wo', 'tp', 'wf', 'mf', 'nh', 'ae']
+
+bn.add(None,[],0)
+
+
